65696e9d2aa0782f77c6ec022083ee677a7acbe9
Jhalak Mahansaria <jhalakmahansaria1@gmail.com>
2026-01-24 10:34:16 +0530

    Fix SCSS import path casing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
-const App = () => {
-  return <div>Hello World</div>;
-};
+import React from 'react'
+import "./App.scss"
+import Navbar from './components/navbar/Navbar.jsx'
+import Sidebar from './components/sidebar/Sidebar.jsx'
+import Hero from './components/hero/Hero.jsx'
+import Parallax from './components/parallax/Parallax.jsx'
+import Services from './components/services/Services.jsx'
+import Portfolio from './components/portfolio/Portfolio.jsx'
+import Contact from './components/contact/Contact.jsx'
+import Cursor from './components/cursor/Cursor.jsx'
 
-export default App;
+const App = () => {
+  return (
+    <div>
+      <Cursor/>
+      <Sidebar />
+
+      <section id='Homepage'>
+        <Navbar />
+        <Hero />
+      </section>
+
+      <section id='Services'>
+        <Parallax type="services"/>
+      </section>
+
+      <section>
+        <Services/>
+      </section>
+
+      <section id='Portfolio'>
+        <Parallax type="portfolio"/>
+      </section>
+
+      <Portfolio/>
+      
+      <section id='Contact'>
+        <Contact/>
+      </section>
+      
+    </div>
+  )
+}
+
+export default App
